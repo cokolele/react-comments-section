1a8cdbd150b74939724347475aff0c197e9fb040
cokolele <lukasdz9457@gmail.com>
2019-05-23 23:46:06 +0200

    fixed api for lan

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const apiBase = (process.env.NODE_ENV == "development" ? "http://localhost:3000/api" : "mojserverktorynemam.sk/apiktoreneexistuje");
+const apiBase = (process.env.NODE_ENV == "development" ? "http://192.168.0.199:3000/api" : "mojserverktorynemam.sk/apiktoreneexistuje");
 
 export { apiBase };
